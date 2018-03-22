e8975524e3b2d94d63b4e2e4e5d09a37acfcbea8
Jesse <jward1990@gmail.com>
2018-03-22 01:22:02 -0400

    added create expense functionality

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import Expense from './Expense';
+import CreateExpense from './CreateExpense';
 
 class Budget extends Component {
     state = {
@@ -19,25 +20,123 @@ class Budget extends Component {
                 amount: "$320.22",
                 category: "Stuff"
             }
-        ]
+        ],
+        showExpenseCreation: false,
+        expenseToCreate: {
+            name: "",
+            date: "",
+            amount: 0,
+            category: ""
+        },
+        idCounter: 3
     }
 
+    //region: Controlled Component functions for creating expense
+
+    onCreateExpenseNameChange = (newName) => {
+        this.setState((currentState) => ({
+            expenseToCreate: {
+                name: newName,
+                date: currentState.expenseToCreate.date,
+                amount: currentState.expenseToCreate.amount,
+                category: currentState.expenseToCreate.category
+            }
+        }));
+    }
+
+    onCreateExpenseDateChange = (newDate) => {
+        this.setState((currentState) => ({
+            expenseToCreate: {
+                name: currentState.expenseToCreate.name,
+                date: newDate,
+                amount: currentState.expenseToCreate.amount,
+                category: currentState.expenseToCreate.category
+            }
+        }));
+    }
+
+    onCreateExpenseAmountChange = (newAmount) => {
+        this.setState((currentState) => ({
+            expenseToCreate: {
+                name: currentState.expenseToCreate.name,
+                date: currentState.expenseToCreate.date,
+                amount: newAmount,
+                category: currentState.expenseToCreate.category
+            }
+        }));
+    }
+
+    onCreateExpenseCategoryChange = (newCategory) => {
+        this.setState((currentState) => ({
+            expenseToCreate: {
+                name: currentState.expenseToCreate.name,
+                date: currentState.expenseToCreate.date,
+                amount: currentState.expenseToCreate.amount,
+                category: newCategory
+            }
+        }));
+    }
+    //endregion
+
+    toggleExpenseCreation = () => {
+        this.setState((currentState) => ({
+            showExpenseCreation: !currentState.showExpenseCreation
+        }));
+    }
+
+    createExpense = () => {
+        this.toggleExpenseCreation();
+
+        this.setState((currentState) => {
+            currentState.expenseToCreate.id = currentState.idCounter;
+
+            return {
+                expenses: currentState.expenses.concat(currentState.expenseToCreate),
+                idCounter: currentState.idCounter + 1,
+                expenseToCreate: {
+                    name: "",
+                    date: "",
+                    amount: 0,
+                    category: ""
+                }
+            }
+        });
+    }
+
+
     render() {
-        
+
         const expensesToDisplay = this.state.expenses.map((expense) =>
-        <Expense
-            key={expense.id}
-            name={expense.name}
-            date={expense.date}
-            amount={expense.amount}
-            category={expense.category}
-         />
-    )
+            <Expense
+                key={expense.id}
+                name={expense.name}
+                date={expense.date}
+                amount={expense.amount}
+                category={expense.category}
+            />
+        )
         return (
             <div>
                 <h2>{this.props.name}</h2>
                 <h3>Spending limit per month: ${this.props.topLimit}</h3>
                 <h3>Total income per month: ${this.props.monthlyIncome}</h3>
+
+                {this.state.showExpenseCreation &&
+                    <CreateExpense
+                        name={this.state.expenseToCreate.name}
+                        date={this.state.expenseToCreate.date}
+                        amount={this.state.expenseToCreate.amount}
+                        category={this.state.expenseToCreate.category}
+                        createExpense={this.createExpense}
+                        expenseNameChanged={this.onCreateExpenseNameChange}
+                        expenseDateChanged={this.onCreateExpenseDateChange}
+                        expenseAmountChanged={this.onCreateExpenseAmountChange}
+                        expenseCategoryChanged={this.onCreateExpenseCategoryChange}
+                    />}
+                {!this.state.showExpenseCreation && (
+                    <button onClick={this.toggleExpenseCreation}>Create Expense</button>
+                )}
+
                 {expensesToDisplay}
             </div>
         )
---
+++ src/components/CreateExpense.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class CreateExpense extends Component {
+    render() {
+        return (
+            <div>
+                <label>
+                    Name:
+                    <input type="text" value={this.props.name} onChange={(e) => this.props.expenseNameChanged(e.target.value)} name="expenseName"/>
+                </label>
+                <label>
+                    Date:
+                    <input type="date" value={this.props.date} onChange={(e) => this.props.expenseDateChanged(e.target.value)} name="expenseDate"/>
+                </label>
+                <label>
+                    Amount:
+                    <input type="text" value={this.props.amount} onChange={(e) => this.props.expenseAmountChanged(e.target.value)} name="expenseAmount"/>
+                </label>
+                <label>
+                    Category:
+                    <input type="text" value={this.props.category} onChange={(e) => this.props.expenseCategoryChanged(e.target.value)} name="expenseCategory"/>
+                </label>
+                <br />
+                {JSON.stringify(this.props)}
+                <button onClick={this.props.createExpense}>Create Expense</button>
+            </div>
+        )
+    }
+}
+
+export default CreateExpense;
